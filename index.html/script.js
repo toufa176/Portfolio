dd3c024fa178b3bc0ad0bbc1203bfae74cf36597
toufa176 <toufaano@gmail.com>
2025-12-25 19:05:11 +0600

    Updated

--- index.html/script.js
+++ index.html/script.js
@@ -59,3 +59,21 @@ skills.forEach(skill => {
     li.textContent = skill;
     skillsList.appendChild(li);
 });
+
+// scroll animation
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry =>{
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.2
+
+});
+sections.forEach(section => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
